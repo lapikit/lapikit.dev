a62d0bb04b60b07efc27d5b17310daabe6ef9967
nycolaide <laurent@nycolaide.dev>
2025-04-21 19:25:23 +0200

    add base script for load docs files on website

---
+++ apps/website/src/lib/scripts/build-docs.mjs
@@ -0,0 +1,29 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { cp } from 'fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Path to the root directory of the project
+const rootDir = path.resolve(__dirname, '../../../../../');
+const sourceDir = path.join(rootDir, 'docs');
+const destDir = path.join(rootDir, 'apps', 'website', 'content');
+
+async function copyDocs() {
+	try {
+		if (!fs.existsSync(sourceDir)) {
+			console.error(`The source folder does not exist: ${sourceDir}`);
+			process.exit(1);
+		}
+
+		await cp(sourceDir, destDir, { recursive: true });
+		console.log(`Successfully copied "docs" folder into "apps/website/content"`);
+	} catch (err) {
+		console.error('Error copying files:', err);
+		process.exit(1);
+	}
+}
+
+copyDocs();
