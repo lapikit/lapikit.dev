626f4e23d4115dc6e6db6f5edf4fe6dec7a7e5e8
nycolaide <laurent@nycolaide.dev>
2025-05-10 18:56:41 +0200

    fix tooltip spacing + prepare to release 0.1.3

--- apps/website/src/lib/components/docs/tooltip/tooltip-density.svelte
+++ apps/website/src/lib/components/docs/tooltip/tooltip-density.svelte
@@ -2,14 +2,14 @@
 	import { Btn, Tooltip } from 'lapikit/components';
 </script>
 
-<Tooltip label="Small tooltip" density="compact" open>
+<Tooltip label="Small tooltip" density="compact">
 	<Btn>Compact</Btn>
 </Tooltip>
 
-<Tooltip label="Default style" open>
+<Tooltip label="Default style">
 	<Btn>Default</Btn>
 </Tooltip>
 
-<Tooltip label="It's large !" density="comfortable" open>
+<Tooltip label="It's large !" density="comfortable">
 	<Btn>Comfortable</Btn>
 </Tooltip>
