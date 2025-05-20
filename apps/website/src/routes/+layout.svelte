516443a1b7dc722dc4dca10868c7a1c28c098e89
nycolaide <laurent@nycolaide.dev>
2025-05-20 14:44:32 +0200

    WIP cancle load all variant font source roboto css

--- apps/website/src/routes/+layout.svelte
+++ apps/website/src/routes/+layout.svelte
@@ -12,24 +12,25 @@
 	import 'mingcute_icon/font/Mingcute.css';
 
 	// fonts
-	import '@fontsource/roboto/100.css';
-	import '@fontsource/roboto/200.css';
-	import '@fontsource/roboto/300.css';
-	import '@fontsource/roboto/400.css';
-	import '@fontsource/roboto/500.css';
-	import '@fontsource/roboto/600.css';
-	import '@fontsource/roboto/700.css';
-	import '@fontsource/roboto/800.css';
-	import '@fontsource/roboto/900.css';
-	import '@fontsource/roboto/100-italic.css';
-	import '@fontsource/roboto/200-italic.css';
-	import '@fontsource/roboto/300-italic.css';
-	import '@fontsource/roboto/400-italic.css';
-	import '@fontsource/roboto/500-italic.css';
-	import '@fontsource/roboto/600-italic.css';
-	import '@fontsource/roboto/700-italic.css';
-	import '@fontsource/roboto/800-italic.css';
-	import '@fontsource/roboto/900-italic.css';
+	import '@fontsource/roboto';
+	// import '@fontsource/roboto/100.css';
+	// import '@fontsource/roboto/200.css';
+	// import '@fontsource/roboto/300.css';
+	// import '@fontsource/roboto/400.css';
+	// import '@fontsource/roboto/500.css';
+	// import '@fontsource/roboto/600.css';
+	// import '@fontsource/roboto/700.css';
+	// import '@fontsource/roboto/800.css';
+	// import '@fontsource/roboto/900.css';
+	// import '@fontsource/roboto/100-italic.css';
+	// import '@fontsource/roboto/200-italic.css';
+	// import '@fontsource/roboto/300-italic.css';
+	// import '@fontsource/roboto/400-italic.css';
+	// import '@fontsource/roboto/500-italic.css';
+	// import '@fontsource/roboto/600-italic.css';
+	// import '@fontsource/roboto/700-italic.css';
+	// import '@fontsource/roboto/800-italic.css';
+	// import '@fontsource/roboto/900-italic.css';
 
 	import '$lib/styles/app.css';
 	import { Darkmode, RGPD } from 'site-kit';
