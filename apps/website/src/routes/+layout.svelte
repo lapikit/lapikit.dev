ebba1edddd8499733d0f8746e0bfb77ba1a4da53
nycolaide <laurent@nycolaide.dev>
2025-05-20 15:45:45 +0200

    clear legacy code

--- apps/website/src/routes/+layout.svelte
+++ apps/website/src/routes/+layout.svelte
@@ -10,29 +10,11 @@
 
 	// icons
 	import 'mingcute_icon/font/Mingcute.css';
-
 	// fonts
 	import '@fontsource/roboto';
-	// import '@fontsource/roboto/100.css';
-	// import '@fontsource/roboto/200.css';
-	// import '@fontsource/roboto/300.css';
-	// import '@fontsource/roboto/400.css';
-	// import '@fontsource/roboto/500.css';
-	// import '@fontsource/roboto/600.css';
-	// import '@fontsource/roboto/700.css';
-	// import '@fontsource/roboto/800.css';
-	// import '@fontsource/roboto/900.css';
-	// import '@fontsource/roboto/100-italic.css';
-	// import '@fontsource/roboto/200-italic.css';
-	// import '@fontsource/roboto/300-italic.css';
-	// import '@fontsource/roboto/400-italic.css';
-	// import '@fontsource/roboto/500-italic.css';
-	// import '@fontsource/roboto/600-italic.css';
-	// import '@fontsource/roboto/700-italic.css';
-	// import '@fontsource/roboto/800-italic.css';
-	// import '@fontsource/roboto/900-italic.css';
-
+	// css
 	import '$lib/styles/app.css';
+
 	import { Darkmode, RGPD } from 'site-kit';
 	import { App } from 'lapikit/components';
 	import { colorScheme, setColorScheme } from 'lapikit/stores';
