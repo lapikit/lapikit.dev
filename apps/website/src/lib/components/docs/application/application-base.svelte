ca6228156d068bedd54be00d03f0105cccbd396f
nycolaide <laurent@nycolaide.dev>
2025-07-01 18:06:33 +0200

    add new docs for application component

---
+++ apps/website/src/lib/components/docs/application/application-base.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import { App } from 'lapikit/components';
+</script>
+
+<App>your content here</App>
--- apps/website/src/templates/md/template.component.md
+++ apps/website/src/templates/md/template.component.md
@@ -14,6 +14,7 @@ keywords:
   - keyword-1
   - keyword-2
 published: true
+state: 'updated'
 ---
 
 <script>
