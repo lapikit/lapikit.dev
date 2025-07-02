d77661f8dfefceb975fed0271d11211c2c6f4e44
Nycolaide <laurent@nycolaide.dev>
2025-07-02 20:13:30 +0200

    add 2 new documentation alert and appbar

--- apps/website/src/lib/components/docs/appbar/appbar-base.svelte
+++ apps/website/src/lib/components/docs/appbar/appbar-base.svelte
@@ -1,33 +1,7 @@
 <script lang="ts">
-	import { Appbar, List, ListItem, Button } from 'lapikit/components';
+	import { Appbar } from 'lapikit/components';
 </script>
 
-<Appbar classContent="grid grid-cols-3" density="comfortable">
+<Appbar classContent="grid grid-cols-3">
 	<p>Lapikit</p>
-	<List orientation="horizontal" rounded="full" density="compact" class="mr-auto ml-auto lg:block">
-		<ListItem href="/">Home</ListItem>
-		<ListItem href="/docs">Docs</ListItem>
-		<ListItem href="https://github.com/Nycolaide/lapikit" target="_blank">GitHub</ListItem>
-	</List>
-	<div class="justify-end gap-3 lg:flex">
-		<Button density="comfortable">Get started</Button>
-	</div>
 </Appbar>
-
-<!-- <div class="container-exemple">
-	<Navbar orientation="vertical">
-		<Button>Home</Button>
-		<Spacer />
-		<Button>Setting</Button>
-		<Button>Administration</Button>
-	</Navbar>
-</div>
-
-<style>
-	.container-exemple {
-		width: 100%;
-		background-color: red;
-		height: 500px;
-		display: flex;
-	}
-</style> -->
---
+++ apps/website/src/lib/components/docs/appbar/appbar-density.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { Appbar } from 'lapikit/components';
+</script>
+
+<Appbar classContent="grid grid-cols-3" density="comfortable">
+	<p>Lapikit</p>
+</Appbar>
+
+<Appbar classContent="grid grid-cols-3" density="default">
+	<p>Lapikit</p>
+</Appbar>
+
+<Appbar classContent="grid grid-cols-3" density="compact">
+	<p>Lapikit</p>
+</Appbar>
