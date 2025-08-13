5df12b81ddf88c7fb740ef5c10e2001833e09c2c
Nycolaide <laurent@nycolaide.dev>
2025-08-13 11:23:48 +0200

    fix responsive exemple code button

--- apps/website/src/lib/components/docs/button/button-color.svelte
+++ apps/website/src/lib/components/docs/button/button-color.svelte
@@ -2,8 +2,10 @@
 	import { Button } from 'lapikit/components';
 </script>
 
-<Button class="primary">Primary</Button>
-<Button class="secondary" variant="outline">Secondary</Button>
-<Button class="tertiary" variant="text">Tertiary</Button>
-<Button color="on-primary" background="primary">Primary variables</Button>
-<Button color="cyan" variant="outline">Outline colors</Button>
+<div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+	<Button class="primary">Primary</Button>
+	<Button class="secondary" variant="outline">Secondary</Button>
+	<Button class="tertiary" variant="text">Tertiary</Button>
+	<Button color="on-primary" background="primary">Primary variables</Button>
+	<Button color="cyan" variant="outline">Outline colors</Button>
+</div>
--- apps/website/src/lib/components/docs/button/button-state.svelte
+++ apps/website/src/lib/components/docs/button/button-state.svelte
@@ -2,10 +2,11 @@
 	import { Button } from 'lapikit/components';
 </script>
 
-<Button info>Info</Button>
-<Button success>Success</Button>
-<Button warning>Warning</Button>
-<Button error>Error</Button>
-<br />
-<Button variant="outline" info>Info</Button>
-<Button variant="text" success>Success</Button>
+<div class="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
+	<Button info>Info</Button>
+	<Button success>Success</Button>
+	<Button warning>Warning</Button>
+	<Button error>Error</Button>
+	<Button variant="outline" info>Info</Button>
+	<Button variant="text" success>Success</Button>
+</div>
