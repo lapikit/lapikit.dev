2f00c9e719802108855ff7466b59c3e52c0979ff
Nycolaide <laurent@nycolaide.dev>
2025-09-19 15:59:04 +0200

    WIP new navigation bar

--- apps/website/src/lib/components/navbar/global.svelte
+++ apps/website/src/lib/components/navbar/global.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
-	import { Appbar } from 'lapikit/components';
+	import { Appbar, Toolbar } from 'lapikit/components';
+	import { fly } from 'svelte/transition';
 	import type { GlobalProps } from './types';
 
 	let { url = '' }: GlobalProps = $props();
+	let visible = $state(true);
 </script>
 
 <Appbar
@@ -11,4 +13,20 @@
 	rounded="xl"
 >
 	{url}
+
+	<label>
+		<input type="checkbox" bind:checked={visible} />
+		visible
+	</label>
 </Appbar>
+
+{#if visible}
+	<div
+		transition:fly={{ y: -20, duration: 500 }}
+		class="fixed z-80 md:top-[4rem] md:left-[6vw] md:w-[88vw]!"
+	>
+		<Toolbar class=" " classContent="mt-[13px]" background="green" rounded="xl" density="compact">
+			toolbar content
+		</Toolbar>
+	</div>
+{/if}
