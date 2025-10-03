fe1ceec077196a9df61fe7c70868ade470d06748
Nycolaide <laurent@nycolaide.dev>
2025-10-03 14:10:48 +0200

    add subfooter on lapikit.dev

--- apps/website/src/routes/+page.svelte
+++ apps/website/src/routes/+page.svelte
@@ -742,7 +742,9 @@
 {#if PUBLIC_DEV_MODE == 'true'}
 	<section id="support-lapikit">
 		<div>
-			<div class="grid gap-8 text-center md:grid-cols-2 md:text-left">
+			<div
+				class="mx-auto grid w-full max-w-(--ui-container) gap-8 px-4 py-16 text-center sm:px-6 sm:py-24 md:grid-cols-2 md:text-left lg:px-8 lg:py-32"
+			>
 				<div>
 					<h2 class="text-3xl font-bold sm:text-4xl lg:text-5xl">
 						You like <span>Lapikit</span>
@@ -801,7 +803,33 @@
 	<section id="lapikit-is-lapikit">
 		<div>
 			<div class="grid gap-4 text-center">
-				<p>It's not magic, it's Lapikit</p>
+				<p class="text-[1.75rem] font-bold opacity-15">It's not magic, it's Lapikit</p>
+			</div>
+		</div>
+	</section>
+{/if}
+
+{#if PUBLIC_DEV_MODE == 'true'}
+	<section id="lets-go-explore">
+		<div>
+			<div
+				class="mx-auto w-full max-w-(--ui-container) px-4 py-16 text-center sm:px-6 sm:py-24 lg:px-8 lg:py-32"
+			>
+				<p class="text-2xl font-bold sm:text-3xl lg:text-4xl">Start exploring Lapikit</p>
+				<p class="mt-6 sm:text-lg">Design system, themes, components, all in one lib</p>
+				<Button
+					class="mt-8"
+					href="/docs/components"
+					rounded="full"
+					density="comfortable"
+					background="accent-primary"
+					color="white"
+				>
+					Discover the components
+					{#snippet append()}
+						<Icon size="xl" icon="mgc_arrow_right_fill" />
+					{/snippet}
+				</Button>
 			</div>
 		</div>
 	</section>
