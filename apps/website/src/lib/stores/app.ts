8c79d20cc527d65b3fd282bbe0267aee5874f557
Nycolaide <laurent@nycolaide.dev>
2025-11-03 19:39:40 +0100

    clear legacy env variables + update ci

--- apps/website/src/lib/stores/app.ts
+++ apps/website/src/lib/stores/app.ts
@@ -1,5 +1,3 @@
-//environment
-import { PUBLIC_DEV_MODE } from '$env/static/public';
 import { writable, readable } from 'svelte/store';
 
 // internal
@@ -7,7 +5,6 @@ export const mode = writable('development');
 export const search = writable(false);
 export const sectionSelected = writable<string>('base');
 
-export const developmentMode = writable(PUBLIC_DEV_MODE == 'true' ? true : false);
 export const packageManager = writable('npm');
 
 // user device
