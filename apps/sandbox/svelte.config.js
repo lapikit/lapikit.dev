41323984bda72b13e05b6fb4b0a85241a7e122f7
nycolaide <laurent@nycolaide.dev>
2025-12-10 09:17:05 +0100

    integrate new preprocess lapikit on package

--- apps/sandbox/svelte.config.js
+++ apps/sandbox/svelte.config.js
@@ -1,12 +1,12 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { lapikitPreprocessor } from 'lapikit/labs/vite';
+import { lapikitPreprocess } from 'lapikit/labs/preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: [vitePreprocess(), lapikitPreprocessor()],
+	preprocess: [vitePreprocess(), lapikitPreprocess()],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
