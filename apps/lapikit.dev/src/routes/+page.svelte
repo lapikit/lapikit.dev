5ad21c4dc1a1684b75419ee26cb645f379fea265
Nycolaide <laurent@nycolaide.dev>
2026-01-02 23:24:58 +0100

    WIP create new preprocess lapikit

--- apps/lapikit.dev/src/routes/+page.svelte
+++ apps/lapikit.dev/src/routes/+page.svelte
@@ -5,10 +5,6 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<ul>
-    <li><a href="/">Home</a></li>
-    <li><a href="/ping">Ping</a></li>
-</ul>
 
 <h2>Stats Lapikit</h2>
 <ul>
--- apps/lapikit.dev/src/routes/ping/+page.svelte
+++
@@ -1,4 +0,0 @@
-<a href="/">Return</a>
-
-<h1>Hello, this is ping</h1>
-<p>This is the ping page.</p>
