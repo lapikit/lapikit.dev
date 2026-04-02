ca087a97b5f44ce72de007db056c373778dc41fa
Nycolaide <laurent@nycolaide.dev>
2026-04-02 22:14:03 +0200

    remove last modification

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,3 @@
-<script lang="ts">
-	import demoImg from '$lib/assets/images/demo.webp?enhanced';
-</script>
-
-<svelte:head>
-	<link rel="preload" as="image" href={demoImg.img.src} fetchpriority="high" />
-</svelte:head>
-
 <p>content</p>
 
 <enhanced:img src="$lib/assets/images/demo.webp" alt="gnome test" sizes="100vw" fetchpriority="high" loading="eager" />
