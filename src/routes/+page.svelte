9e96bce679626c583c69c54110074464edc963b5
Nycolaide <laurent@nycolaide.dev>
2026-04-02 22:04:09 +0200

    test optimize resource load duration and delay

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,11 @@
+<script lang="ts">
+	import demoImg from '$lib/assets/images/demo.webp?enhanced';
+</script>
+
+<svelte:head>
+	<link rel="preload" as="image" href={demoImg.img.src} fetchpriority="high" />
+</svelte:head>
+
 <p>content</p>
 
 <enhanced:img src="$lib/assets/images/demo.webp" alt="gnome test" sizes="100vw" fetchpriority="high" loading="eager" />
