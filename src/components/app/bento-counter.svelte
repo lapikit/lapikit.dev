4365a37a6a29782839ea1ec89eed09015131cd0c
Nycolaide <laurent@nycolaide.dev>
2026-07-24 23:11:58 +0200

    WIP search idea to new homepage UI

---
+++ src/components/app/bento-counter.svelte
@@ -0,0 +1,108 @@
+<script lang="ts">
+	import { Star } from 'lucide-svelte';
+</script>
+
+<div id="bento-counter">
+	<kit:card class="card-integrations" density="comfortable">
+		<kit:card-content>
+			<kit:chip>
+				<kit:icon><Star /></kit:icon>
+			</kit:chip>
+		</kit:card-content>
+		<kit:card-container>
+			<kit:card-title class="lpk-title-2xl">Integrations</kit:card-title>
+			<kit:card-content>Use your favorites builder with lapikit</kit:card-content>
+		</kit:card-container>
+	</kit:card>
+	<kit:card class="card-customize" density="comfortable">
+		<kit:card-content> </kit:card-content>
+		<kit:card-container>
+			<kit:card-title class="lpk-title-2xl">Customize</kit:card-title>
+			<kit:card-content>Support themes and design system with global variables</kit:card-content>
+		</kit:card-container>
+	</kit:card>
+	<kit:card class="card-components" density="comfortable">
+		<kit:card-title class="lpk-title-2xl"> Components</kit:card-title>
+		<kit:card-container>
+			<div class="counter">25</div>
+			<kit:card-content>Optimized for svelte and sveltekit</kit:card-content>
+		</kit:card-container>
+	</kit:card>
+	<kit:card class="card-hooks" density="comfortable">
+		<kit:card-content>
+			<div class="counter">3</div>
+		</kit:card-content>
+		<kit:card-container>
+			<kit:card-title class="lpk-title-2xl">Hooks and actions</kit:card-title>
+			<kit:card-content>More utils for facility your developpment</kit:card-content>
+		</kit:card-container>
+	</kit:card>
+	<kit:card class="card-opensource" density="comfortable">
+		<kit:card-content> </kit:card-content>
+		<kit:card-container>
+			<kit:card-title class="lpk-title-2xl">Open Source</kit:card-title>
+			<kit:card-content>Lapikit is 100% Open Source. <br /> Use license MIT</kit:card-content>
+		</kit:card-container>
+	</kit:card>
+	<kit:card class="card-lowdeps" density="comfortable">
+		<kit:card-content> </kit:card-content>
+		<kit:card-container>
+			<kit:card-title class="lpk-title-2xl">Low dependencies for long support</kit:card-title>
+			<kit:card-content>
+				It is developed using 67 external packages 54% fewer than the other major component
+				libraries to ensure better support and security.</kit:card-content
+			>
+		</kit:card-container>
+	</kit:card>
+</div>
+
+<style>
+	#bento-counter {
+		display: grid;
+		grid-template-columns: repeat(4, 1fr);
+		grid-auto-rows: minmax(180px, auto);
+		gap: 1rem;
+		grid-template-areas:
+			'integrations components hooks     opensource'
+			'customize    components lowdeps   lowdeps';
+	}
+
+	#bento-counter > :global(.card-integrations),
+	#bento-counter > :global(.card-customize),
+	#bento-counter > :global(.card-hooks),
+	#bento-counter > :global(.card-opensource),
+	#bento-counter > :global(.card-lowdeps) {
+		display: grid;
+		grid-template-rows: 1fr auto;
+	}
+
+	#bento-counter > :global(.card-components) {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+	}
+
+	.counter {
+		font-size: calc(var(--kit-font-xl) * 3.5);
+	}
+
+	/* grid area */
+	#bento-counter > :global(.card-integrations) {
+		grid-area: integrations;
+	}
+	#bento-counter > :global(.card-components) {
+		grid-area: components;
+	}
+	#bento-counter > :global(.card-hooks) {
+		grid-area: hooks;
+	}
+	#bento-counter > :global(.card-opensource) {
+		grid-area: opensource;
+	}
+	#bento-counter > :global(.card-customize) {
+		grid-area: customize;
+	}
+	#bento-counter > :global(.card-lowdeps) {
+		grid-area: lowdeps;
+	}
+</style>
---
+++ src/routes/page-v2.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+	import BentoCounter from '$components/app/bento-counter.svelte';
+
+	import LapikitLogo from '$lib/assets/images/lapikit.webp?enhanced';
+	import SvelteLogo from '$lib/assets/images/svelte.webp?enhanced';
+
+	// states
+	let shellCommand: string = $state('npm install -D lapikit');
+
+	let shellList = {
+		npm: 'npm install --save-dev lapikit',
+		yarn: 'yarn add -D lapikit',
+		bun: 'bun add -D lapikit'
+	};
+</script>
+
+<section>
+	<div>
+		<div>
+			<kit:chip size="xs">This app was made with Lapikit</kit:chip>
+			<div>
+				<enhanced:img src={LapikitLogo} alt="Lapikit logo" class="no-select w-9.5 min-w-9.5" />
+				<h1>Simple, optimized components for Svelte</h1>
+				<enhanced:img src={SvelteLogo} alt="Lapikit logo" class="no-select w-9.5 min-w-9.5" />
+			</div>
+			<p>
+				A library of accessible, high-performance, versatile components that let you develop fast,
+				fully customizable interfaces.
+			</p>
+			<p>
+				<kit:chip>svelte</kit:chip> and <kit:chip>sveltekit</kit:chip>
+			</p>
+			<div>
+				<kit:btn>Discover the power</kit:btn>
+				<kit:btn>Install Guide</kit:btn>
+			</div>
+			<div>
+				<kit:btn>{shellCommand}</kit:btn>
+				<div>
+					<kit:btn onclick={() => (shellCommand = shellList['npm'])}>npm</kit:btn>
+					<kit:btn onclick={() => (shellCommand = shellList['yarn'])}>yarn</kit:btn>
+					<kit:btn onclick={() => (shellCommand = shellList['bun'])}>bun</kit:btn>
+				</div>
+			</div>
+		</div>
+		<div>2</div>
+	</div>
+</section>
+
+<BentoCounter />
+
+<style>
+	section {
+		height: 100vh;
+		width: 100%;
+		background-image:
+			linear-gradient(var(--kit-color-fill) 1px, transparent 1px),
+			linear-gradient(90deg, var(--kit-color-fill) 1px, transparent 1px),
+			linear-gradient(var(--kit-color-fill) 0.5px, transparent 0.5px),
+			linear-gradient(90deg, var(--kit-color-fill) 0.5px, var(--kit-color-surface) 0.5px);
+		background-size:
+			100px 100px,
+			100px 100px,
+			20px 20px,
+			20px 20px;
+		background-position:
+			-1px -1px,
+			-1px -1px,
+			-0.5px -0.5px,
+			-0.5px -0.5px;
+	}
+</style>
